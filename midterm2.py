7919e3a43302840fd599aa3f02e5ae796cbd1c7c
Tom Pianet <tom.pianet.us@gmail.com>
2024-02-23 11:48:24 +0100

    zee

--- midterm2.py
+++ midterm2.py
@@ -4,23 +4,19 @@ def words_b(filename):
     :param filename:  name of the file
     :return: nothing
     """
-    #open the file
-    return
     punctuation = ",!?.\n"
-
     with open(filename, "r") as file:
         # go over the line by file
         for line in file:
             #replace each punctuation mark with nothing
-            for p in punctuation:
-                line = line.replace(p, "")
+            for p in punctuation :
+                line = line.replace(p," ")
             #get the words in the line
-            words = line.split("")
-
+            words = line.split(" ")
             for word in words:
                 if len(word) == 3 and word.startswith("b"):
                     print(word)
     return
 
 #call the function
-words_b("text.txt")
+words_b("text.txt.py")
