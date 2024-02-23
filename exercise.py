4185d466cada97248c7a4e7d3b620e57b69b1f3b
Tom Pianet <tom.pianet.us@gmail.com>
2024-02-23 11:08:29 +0100

    zee

--- exercise.py
+++ exercise.py
@@ -5,12 +5,17 @@ def m6() :
     """
     while True:
         try :
+            #first we need to read a number
             number = input("Please give me a multiple of 6 number")
             number = int(number)
 
         except ValueError :
+            #if the number is not valid print an error and retry
             print("This is not a number.Retry")
             continue
+        #check if the number is a multiple of 6
         if number % 6 == 0:
+            # return the value
             return number
+        # print and error and try again
         print("That number is not a multiple of 6. Retry")
